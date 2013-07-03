5393e5e54fae466c7499c5ebd222e5a6014ac87e
rai200890@gmail.com <rai200890@gmail.com@d9c1df84-da40-0b3d-a3e3-741a724da7c3>
2013-07-03 14:44:31 +0000

    merge dos 2 projetos.

--- scripts/sources/MemoriaVirtualViewModel.js
+++ scripts/sources/MemoriaVirtualViewModel.js
@@ -6,10 +6,12 @@ function MemoriaVirtualViewModel(params){
         tam_ms: 4096,
         tam_quadro : 64
     }
+    var n_quadros = Math.ceil(params.tam_mp/params.tam_quadro);
 
     self.novoProcesso = ko.observable(new Processo());
     self.pedidos = ko.observableArray([]);
     self.novoPedido = ko.observable(new Pedido({}));
+    self.tpSelecionada = ko.observable(null);
     
     self.tpSelecionada = ko.observable(new TP(null, 0));
 
@@ -24,10 +26,15 @@ function MemoriaVirtualViewModel(params){
         var processo_id = pedido.processo_id;
         var end_virtual = parseInt(pedido.endereco_virtual,2);
         var escrita = pedido.escrita;
+        var memoriaVirtual = self.memoriaVirtual();
+        memoriaVirtual.buscar(processo_id, end_virtual, escrita);
+        self.memoriaVirtual(memoriaVirtual);
     }
     
     self.carregarProcesso = function(){
-        self.memoriaVirtual.carregarProcesso(self.novoProcesso());
+        var memoriaVirtual = self.memoriaVirtual();
+        memoriaVirtual.carregarProcesso(self.novoProcesso());
+        self.memoriaVirtual(memoriaVirtual);   //o q é isso?
         self.novoProcesso(new Processo());
     }
 }
--- scripts/sources/models/MemoriaVirtual.js
+++ scripts/sources/models/MemoriaVirtual.js
@@ -1,59 +1,106 @@
 function MemoriaVirtual(params){
     var self = this;
-    var n_quadros = Math.ceil(params.tam_mp/params.tam_quadro);
-    
-    self.tlb = new TLB(5);
-    self.mp = new MP(n_quadros);
-    self.ms = new MS(params.tam_ms);
+    self.tam_mp = parseInt(params.tam_mp);
+    self.tam_ms = parseInt(params.tam_ms);
+    self.tam_quadro = parseInt(params.tam_quadro)
+    self.n_quadros = Math.ceil(self.tam_mp/self.tam_quadro);
+
+    self.tlb = new TLB();
+    self.mp = new MP(self.n_quadros);
+    self.ms = new MS(self.tam_ms);
     self.tps = ko.observableArray([]);
-    
-    
+
+    self.indexOfTP = function(processo_id){
+        var resposta = null;
+        $.each(self.tps, function(index, tp){
+            if (tp.id == processo_id)
+                resposta = index;
+        });
+        return resposta;
+    }
+
     self.carregarProcesso = function(processo){
-        self.ms.carregarPaginas(processo);
+        self.ms.carregarProcesso(processo);
         var n_paginas = Math.ceil(processo.tamanho/params.tam_quadro);
         self.tps.push(new TP(processo.id, n_paginas));
     }
-    
-    self.processarPedido = function(processo_id, end_virtual, escrita){
-        var tp = self.getTP(processo_id);
-        if(tp){
-            if (escrita)
-                self.escrever(tp, end_virtual);
-            else
-                self.buscar(tp,end_virtual);
-        }else
-            alert("Processo não existe!")
+
+    self.buscaEntradaProcessoTLB = function(processo_id, end_virtual){
+        var n_pagina = self.obtemPaginaEndVirtual(end_virtual)
+        var entradaTpProcesso = null;
+        $.each(self.tlb(), function(index, entrada_tlb){
+            if(entrada_tlb.processo_id == processo_id && entrada_tlb.n_pagina == n_pagina){
+                entradaTpProcesso = entrada_tlb.entrada_tp;
+                return;
+            }
+        })
+        return entradaTpProcesso;
     }
-}
-self.escrever = function(tp, end_virtual){
-    if(escrita){
-        tp.modificaEntrada(n_pagina);
+
+    self.buscaEntradaProcessoTP = function(processo_id, end_virtual){
+        var n_pagina = self.obtemPaginaEndVirtual(end_virtual)
+        var entradaTpProcesso = null;
+        $.each(self.tps(), function(index, tp){
+            if(tp.processo_id == processo_id){
+                entradaTpProcesso = tp.entradas[n_pagina];
+                return;
+            }
+        })
+        self.atualizaTLB(tp_entrada);
+        return entradaTpProcesso;
     }
 }
 
-self.buscar = function(tp, end_virtual){
-    var n_pagina = Math.floor(end_virtual/params.tam_quadro);
-    var entrada_tp = self.tlb.getEntradaTP(processo_id, n_pagina);
-    if (!entrada_tp){
-        alert("TLB Miss!");
-        //verificar se a página é válida
-        entrada_tp = tp.entradas[n_pagina];
-        self.tlb.carregar(processo_id, n_pagina, entrada_tp);
+    self.retornaQuadro = function(entrada_tp){
+        var quadro;
+        if(entrada_tp.p()){
+            quadro = self.mp.quadros[entrada_tp.n_quadro];
+        }
+        else{
+            alert('Page Fault!');
+            self.carregaPaginaMP(self.retornaPaginaMS());
+        }
+        return quadro;
     }
-    if (entrada_tp.p) {
-       
-    }else
-        alert("Falta de Página");
-    var indexQuadroLivre = self.mp.getQuadroLivre();
-    if (indexQuadroLivre != null){
-        var entrada = tp.entradas[n_pagina];
-        entrada.n_quadro = indexQuadroLivre;
-        entrada.p = true;
-        tp.entradas[n_pagina] = entrada;
-        self.tps[indexTP] = tp;
-        var quadro = self.mp.quadros[indexQuadroLivre];
-        quadro.u = true;
-        self.mp.quadros[indexQuadroLivre] = quadro;
-    }else
-        alert("Processo Não Existe");
+
+    self.obtemPaginaEndVirtual = function(end_virtual){
+        return Math.floor(end_virtual/self.tam_quadro);
+    }
+
+    self.obtemOffSetEndVirtual = function(end_virtual){
+        return end_virtual % self.tam_quadro;
+    }
+//
+//    self.buscar = function(processo_id, end_virtual, escrita){
+//        var indexTP = self.indexOfTP(processo_id);
+//        var tp = self.tps[indexTP];
+//        if(tp){
+//            var n_pagina =
+//            var entrada_tp = tp.entradas[n_pagina];
+//            if (entrada_tp.p) {
+//                if(escrita){
+//                    tp.modificaEntrada(n_pagina);
+//                    //grava no quadro na MP
+//                    //rever
+//
+//                }
+//            }else
+//                alert("Falta de Página");
+//            var indexQuadroLivre = self.mp.getQuadroLivre();
+//            if (indexQuadroLivre != null){
+//                var entrada = tp.entradas[n_pagina];
+//                entrada.n_quadro = indexQuadroLivre;
+//                entrada.p = true;
+//                tp.entradas[n_pagina] = entrada;
+//                self.tps[indexTP] = tp;
+//                var quadro = self.mp.quadros[indexQuadroLivre];
+//                quadro.u = true;
+//                self.mp.quadros[indexQuadroLivre] = quadro;
+//            }else {
+//                //chama a politica de substituiçao
+//                //se o quadro foi modificado, faz o swap-out da pagina
+//            }
+//        }else
+//            alert("Processo Não Existe");
+//    }
 }
