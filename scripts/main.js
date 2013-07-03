bddd36bfc5c468be77dac6bc45b67da62cf99da5
rai200890@gmail.com <rai200890@gmail.com@d9c1df84-da40-0b3d-a3e3-741a724da7c3>
2013-07-03 15:53:21 +0000

    Modificação na tela.

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,6 @@ require(["sources/MemoriaVirtualViewModel","sources/models/Pedido","sources/mode
     $(document).ready(function(){
         viewmodel = new MemoriaVirtualViewModel();
         ko.applyBindings(viewmodel);
+        log = console.log;
     });
 });
