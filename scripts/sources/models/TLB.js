a7123e0f910b6e26616957cd78ec7fc3beeefa08
rai200890@gmail.com <rai200890@gmail.com@d9c1df84-da40-0b3d-a3e3-741a724da7c3>
2013-07-04 00:55:55 +0000

    Modificação na tela.

--- scripts/sources/models/TLB.js
+++ scripts/sources/models/TLB.js
@@ -1,7 +1,7 @@
 function EntradaTLB(params){
     this.processo_id = ko.observable((params.processo_id || ""));
     this.n_pagina =  ko.observable((params.n_pagina || null));
-    this.entrada_tp  =  ko.observable(params.entrada_tp || new EntradaTP());
+    this.entrada_tp = ko.observable(params.entrada_tp || new EntradaTP());
 }
 
 function TLB(n_entradas){
@@ -17,19 +17,11 @@ function TLB(n_entradas){
 
     self.carregaEntrada = function(processo_id,n_pagina,entrada_tp){
         proxEntrada = (proxEntrada + 1) % self.n_entradas;
-        self.entradas[proxEntrada] = new EntradaTLB({
-            processo_id: processo_id,
-            n_pagina: n_pagina,
-            entrada_tp: entrada_tp});
+        var entrada_tlb = self.entradas[proxEntrada];
+        entrada_tlb.processo_id(processo_id);
+        entrada_tlb.n_pagina(n_pagina);
+        entrada_tlb.entrada_tp(entrada_tp);
     }
 
-    self.buscarPaginaVirtual = function(processo_id, n_pagina){
-        var pagina = null;
-        $.each(self.entradas, function(i,entrada){
-            if(entrada.n_pagina() == n_pagina)
-                pagina = entrada.pagina;
-        });
-        return pagina;
-    }
     self.inicializar();
 }
